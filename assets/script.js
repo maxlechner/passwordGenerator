ff4dcaeb7b52c52883590940c60178c9369660ca
Max Lechner <mkonlech@gmail.com>
2020-10-24 10:48:21 -0700

    define array values for password

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,15 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// set password values in a set of arrays
+var availableChars = {
+  upperChar: ["A","B","C","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"],
+  lowerChar: ["a","b","c","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
+  specialChar: ["!","@","#","$","%","^","&","*","(",")","_","-","+","=",";",":","?","<",">"],
+  numbers: [0,1,2,3,4,5,6,7,8,9]
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
