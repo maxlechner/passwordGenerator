a075b5ce2f4dff886910066d998697edb5c3b60f
Max Lechner <mkonlech@gmail.com>
2020-10-24 13:13:16 -0700

    add validation for the required character types.

--- assets/script.js
+++ assets/script.js
@@ -17,6 +17,9 @@ function generatePassword() {
   // set empty string for password to append values in
   var password = "";
 
+  // define password follow to ensure that at least of each character is included
+  var pwdFollow = [];
+
   // validate the input is a number between 8 and 128, exit with an error message if it is not.
   if ( !(totalCharacters >= 8 || totalCharacters <= 128 || totalCharacters === NaN)) {
     alert("please use a numeric value between 8 and 128.");
@@ -32,32 +35,44 @@ function generatePassword() {
     // ensure that upper characters are included in the character set variable
     if (upperRequired === true) {
       charSet.push(...upperChar);
+      let upperAdd = upperChar[Math.floor(Math.random() *upperChar.length)];
+      pwdFollow.push(upperAdd);
     }
 
+    console.log(pwdFollow);
     console.log(charSet);
   
   var lowerRequired = confirm("Include lower case letters?");
     // ensure that lower characters are included in the character set variable
     if (lowerRequired === true) {
       charSet.push(...lowerChar);
+      let lowerAdd = lowerChar[Math.floor(Math.random() *lowerChar.length)];
+      pwdFollow.push(lowerAdd);
     }
     
+    console.log(pwdFollow);
     console.log(charSet);
 
   var specialRequired = confirm("Include special characters (i.e !@#$% etc.)?");
     // ensure that speical characters are included in the character set variable
     if (specialRequired === true) {
       charSet.push(...specialChar);
+      let specialAdd = specialChar[Math.floor(Math.random() *specialChar.length)];
+      pwdFollow.push(specialAdd);
     }
 
+    console.log(pwdFollow);
     console.log(charSet);
 
   var numbersRequired = confirm("Include numbers?");
     // ensure that numbers are included in the character set variable
     if (numbersRequired === true) {
       charSet.push(...numbers);
+      let numbersAdd = numbers[Math.floor(Math.random() *numbers.length)];
+      pwdFollow.push(numbersAdd);
     }
 
+    console.log(pwdFollow);
     console.log(charSet);
     console.log(charSet.length);
 
@@ -66,15 +81,23 @@ function generatePassword() {
       return;
     }
 
+    // add in values for required set
+    while( password.length < pwdFollow.length) {
+      let pwdFollowSet = pwdFollow[Math.floor(Math.random() *pwdFollow.length)];
+      password += pwdFollowSet;
+      console.log(password)
+    }
+    
     // iterative loop to cycle through the character set into a password variable.
     while( password.length < totalCharacters) {
-
+      
       // select random character from charset
       let randomChar = charSet[Math.floor(Math.random() *charSet.length)];
 
       // append randomchar to password
       password += randomChar;
       console.log(password)
+
     }
 
     console.log(password)
